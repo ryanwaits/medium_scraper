b7e43b9dd62f4d083ace82b884e2b9caa67e7eab
Ryan Waits <ryan.waits@gmail.com>
2016-09-30 14:17:11 -0500

    add gem dependencies

--- lib/medium_scraper.rb
+++ lib/medium_scraper.rb
@@ -1,5 +1,53 @@
 require "medium_scraper/version"
 
 module MediumScraper
-  # Your code goes here...
+  class Post
+      def self.latest handle
+          url = "https://medium.com/@#{handle}/latest"
+          agent = Mechanize.new
+          html = agent.get(url).body
+          doc = Nokogiri::HTML(html)
+
+          scripts = doc.search('//script')
+          tag = scripts[6]
+
+          front_string = "<script>// <![CDATA[\nwindow[\"obvInit\"]("
+          back_string = ")\n// ]]></script>".reverse
+
+          tag = scripts[6].to_s.sub(front_string, '')
+          tag = tag.reverse.to_s.sub(back_string, '').reverse
+
+          blob = JSON.parse(tag)
+
+          blob_post = blob['references']['Post']
+          blob_user = blob['references']['User']
+
+          if blob_post
+              posts = []
+              blob_post.each do |blob|
+                  post = {}
+                  post[:title] = blob[1]['title']
+                  post[:subtitle] = blob[1]['content']['subtitle']
+                  posts << post
+              end
+          end
+
+          if blob_user
+              user = {}
+              blob_user.each do |blob|
+                  user[:name] = blob[1]['name']
+                  user[:username] = blob[1]['username']
+                  user[:url] = "https://medium.com/@#{blob[1]['username']}/"
+                  user[:total_posts] = posts.count if blob_post
+              end
+          end
+
+          object = {
+              user: user,
+             posts: posts 
+          }
+
+          object
+      end
+  end
 end
--- medium_scraper.gemspec
+++ medium_scraper.gemspec
@@ -33,4 +33,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+
+  spec.add_runtime_dependence "mechanize"
+  spec.add_runtime_dependence "nokogiri"
+  spec.add_runtime_dependence "json"
+  spec.add_runtime_dependence "awesome_print"
 end
